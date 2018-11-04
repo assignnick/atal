37ce836890c8952630601a93c45bc68068ae2640
assignnick <assignnick@gmail.com>
2018-11-04 21:35:24 +0400

    1

---
+++ x.java
@@ -0,0 +1,19 @@
+package com.company;
+
+
+
+class x
+{
+   private long f;
+   private int s;
+     x(long f,int s){
+       this.f=f;
+       this.s=s;
+   }
+  public long getf(){
+         return f;
+  }
+    public long gets(){
+        return s;
+    }
+}
