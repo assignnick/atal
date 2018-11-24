f968819835b4d1857888c0be0d095aead4e68db5
assignnick <assignnik@gmail.com>
2018-11-24 09:31:34 +0400

    1

--- Main.java
+++ Main.java
@@ -1,40 +1,103 @@
 package com.company;
 
-
-import java.util.*;
+import java.io.*;
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
+//        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+//        writer.write(df);
+//        writer.flush();
 
-//        InputStreamReader input = new InputStreamReader(System.in);
-//        BufferedReader buffer = new BufferedReader(input);
+        InputStreamReader input = new InputStreamReader(System.in);
+        BufferedReader buffer = new BufferedReader(input);
         // FileInputStream fstream = new FileInputStream("text.txt");
         // BufferedReader buffer = new BufferedReader(new InputStreamReader(fstream));
-//1
+
         Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
-        int k = scanner.nextInt();
+        int n = Integer.parseInt(buffer.readLine());
 
-        int[][] chisla = new int[200005][2];
-        for (int i = 0; i < chisla.length; i++) {
-            chisla[i][1] = i;
-        }
-        ArrayList<Integer> list = new ArrayList<>();
+        String str;
+        long hash;
 
-        Arrays.sort(chisla, new Comparator<int[]>() {
-            @Override
-            public int compare(int[] o1, int[] o2) {
-                return Integer.compare(o2[0], o1[0]);
+        long[][] hashes = new long[n][36];
+        String[] strings = new String[n];
+
+
+        int eighth;
+        int index;
+        for (int i = 0; i < n; ++i) {
+            str = buffer.readLine();
+            strings[i] = str;
+            eighth = 1;
+            index = 0;
+            while (eighth <= str.length()) {
+                for (int j = 0; j <= str.length() - eighth; ++j) {
+                    hash = 0;
+                    for (int k = j, t = 0; k < j + eighth; ++k, ++t) {
+                        //   if ((str.charAt(k) - '\n') == 0) break;
+                        hash += (str.charAt(k) - '.' + 1) * 37 ^ (t+1);
+                    }
+                    hashes[i][index] = hash;
+                    index++;
+                }
+                eighth++;
             }
-        });
+        }
+
+        System.out.println(Arrays.deepToString(hashes));
+        int q = Integer.parseInt(buffer.readLine());
+
+        int[] digits = new int[q];
+        String[] strngs = new String[q];
+
+        int tempstring;
+        for (int i = 0; i < q; ++i) {
+            str = buffer.readLine();
+            hash = 0;
+            tempstring = -1;
+            for (int j = 0; j < str.length(); ++j) {
+                hash += (str.charAt(j) - '.' + 1) * 37 ^ (j+1);
+            }
+
+            System.out.println(str+" " +hash);
+            for (int x = 0; x < n; ++x) {
+                for (int y = 0; y < 36; ++y) {
+                    if (hash == hashes[x][y]) {
+                        digits[i]++;
+                        tempstring = x;
+                        break;
+                    }
+                }
+            }
+
+            if (tempstring != -1)
+                strngs[i] = strings[tempstring];
+            else
+                strngs[i] = "-";
+        }
 
 
+        for (int i = 0; i < q; i++) {
+            System.out.print(digits[i]);
+            System.out.print(" ");
+            System.out.print(strngs[i]);
+            System.out.println(" ");
+        }
 
 
     }
 }
 
+
+//  Arrays.sort(chisla, new Comparator<int[]>() {
+//@Override
+//public int compare(int[] o1, int[] o2) {
+//        return Integer.compare(o2[0], o1[0]);
+//        }
+//        });
 //        int x1,x2,y;
 //        for(int i=0;i<n;i++){
 //            x1=scanner.nextInt();
